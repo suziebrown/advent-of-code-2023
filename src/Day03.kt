21022f1c439451147cc3d049f71aa1ed1ab044e7
Suzie Brown <Suzie.Brown@ghyston.com>
2023-12-22 12:20:45 +0000

    Day 3 part 1 - refactor

--- src/Day03.kt
+++ src/Day03.kt
@@ -7,13 +7,12 @@ fun main() {
         val row: Int,
         val startColumn: Int,
         val endColumn: Int,
-        val isAdjacentToSymbol: Boolean? = null
     )
 
     val numberRegex = "\\d+".toRegex()
     val symbolRegex = "[^\\d.]".toRegex()
 
-    fun checkIsAdjacentToSymbol(row: Int, startColumn: Int, endColumn: Int, input: List<String>): Boolean {
+    fun isAdjacentToSymbol(row: Int, startColumn: Int, endColumn: Int, input: List<String>): Boolean {
         val inputWidth = input[0].length
         val inputHeight = input.size
 
@@ -22,15 +21,18 @@ fun main() {
                 input[row - 1].subSequence(max(startColumn - 1, 0), min(endColumn + 2, inputWidth))
             if (symbolRegex.containsMatchIn(neighboursAbove)) return true
         }
+
         if (row < inputHeight - 1) {
             val neighboursBelow =
                 input[row + 1].subSequence(max(startColumn - 1, 0), min(endColumn + 2, inputWidth))
             if (symbolRegex.containsMatchIn(neighboursBelow)) return true
         }
+
         if (startColumn > 0) {
             val neighbourLeft = input[row].subSequence(startColumn - 1, startColumn)
             if (symbolRegex.containsMatchIn(neighbourLeft)) return true
         }
+
         if (endColumn < inputWidth - 1) {
             val neighbourRight = input[row].subSequence(endColumn + 1, endColumn + 2)
             if (symbolRegex.containsMatchIn(neighbourRight)) return true
@@ -40,11 +42,12 @@ fun main() {
     }
 
     fun mapMatchToSchematicNumber(match: MatchResult, row: Int): SchematicNumber {
-        val value = match.value.toInt()
-        val startColumn = match.range.first
-        val endColumn = match.range.last
-
-        return SchematicNumber(value, row, startColumn, endColumn)
+        return SchematicNumber(
+            value = match.value.toInt(),
+            row = row,
+            startColumn = match.range.first,
+            endColumn = match.range.last
+        )
     }
 
     fun part1(input: List<String>): Int {
@@ -53,12 +56,11 @@ fun main() {
                 numberRegex.findAll(it).toList()
             }.mapIndexed { index, matches ->
                 matches.map { mapMatchToSchematicNumber(it, index) }
-            }.flatten()
+            }.flatten().filter {
+                isAdjacentToSymbol(it.row, it.startColumn, it.endColumn, input)
+            }
 
-        val foo = partNumbers
-            .filter { checkIsAdjacentToSymbol(it.row, it.startColumn, it.endColumn, input) }
-
-        return foo.sumOf { it.value }
+        return partNumbers.sumOf { it.value }
     }
 
     fun part2(input: List<String>): Int {
